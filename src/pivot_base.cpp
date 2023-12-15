666272c7e70f42060e8227cbe46ab4caf20610e8
shunsugar <shun.saito.6t@stu.hosei.ac.jp>
2023-12-15 15:33:58 +0000

    feature pivot_base

--- src/pivot_base.cpp
+++ src/pivot_base.cpp
@@ -1,28 +1,11 @@
-/**
- * Copyright (C) 2023  template
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 //#include <pivot_base/pivot_base.h>
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
-#include <tf2_ros/transform_listener.h>
 #include <costmap_2d/costmap_2d_ros.h>
 #include <base_local_planner/trajectory_planner_ros.h>
 #include <navfn/navfn_ros.h>
 
-enum class NavState{
+enum class PivotState {
   STANDBY,
   WAIT_PLAN,
   MOVING
@@ -30,32 +13,27 @@ enum class NavState{
 
 class PivotBase {
 public:
-//#if ROS_VERSION_MINIMUM(1,14,0)
-  PivotBase() : tfBuffer(), tfListener(tfBuffer), global_costmap_("global_costmap", tfBuffer), local_costmap_("local_costmap", tfBuffer) {
-//#else
 //  PivotBase() : global_costmap_("global_costmap", tf_), local_costmap_("local_costmap", tf_) {
-//#endif
-    twist_pub_ = nh_.advertise<geometry_msgs::Twist>("/dtw_robot1/diff_drive_controller/cmd_vel", 10);
-    goal_sub_ = nh_.subscribe("/move_base_simple/goal", 10, &PivotBase::goalCallback, this);
+  PivotBase() : tfBuffer(), tfListener(tfBuffer), global_costmap_("global_costmap", tfBuffer), local_costmap_("local_costmap", tfBuffer) {
+    vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+//    goal_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>("goal", 1, &PivotBase::goalCB, this);
+    goal_sub_ = nh_.subscribe("/move_base_simple/goal", 10, &PivotBase::goalCB, this);
+    
     global_planner_.initialize("global_planner", &global_costmap_);
-//#if ROS_VERSION_MINIMUM(1,14,0)
-    local_planner_.initialize("local_planner", &tfBuffer, &local_costmap_);
-//#else
 //    local_planner_.initialize("local_planner", &tf_, &local_costmap_);
-//#endif
-
-    nav_state_ = NavState::STANDBY;
-    timer_ = nh_.createTimer(ros::Duration(0.2), &PivotBase::timerCallback, this);
+    local_planner_.initialize("local_planner", &tfBuffer, &local_costmap_);
+    state_ = PivotState::STANDBY;
+    timer_ = nh_.createTimer(ros::Duration(0,2), &PivotBase::timerCB, this);
   }
-
-  void goalCallback(const geometry_msgs::PoseStamped msg){
+  
+  void goalCB(const geometry_msgs::PoseStamped msg){
     ROS_INFO("recieve");
     last_pose_ = msg;
-    nav_state_ = NavState::WAIT_PLAN;
+    state_ = PivotState::WAIT_PLAN;
   }
 
-  void timerCallback(const ros::TimerEvent& e){
-    if (nav_state_ == NavState::WAIT_PLAN){
+  void timerCB(const ros::TimerEvent& e){
+    if (state_ == PivotState::WAIT_PLAN){
       ROS_INFO("PLAN");
 
       geometry_msgs::PoseStamped source_pose;
@@ -77,58 +55,56 @@ public:
     
       if (!global_planner_.makePlan(start, last_pose_, last_global_plan_)){
         ROS_WARN("global plan fail");
-        nav_state_ = NavState::STANDBY;
+        state_ = PivotState::STANDBY;
         return;
       }
       local_planner_.setPlan(last_global_plan_);
       geometry_msgs::Twist cmd_vel;
       if (local_planner_.isGoalReached()){
         ROS_INFO("reach");
-        twist_pub_.publish(cmd_vel);
-        nav_state_ = NavState::STANDBY;
+        vel_pub_.publish(cmd_vel);
+        state_ = PivotState::STANDBY;
         return;
       }
       local_planner_.computeVelocityCommands(cmd_vel);
-      twist_pub_.publish(cmd_vel);
-      nav_state_ = NavState::MOVING;
+      vel_pub_.publish(cmd_vel);
+      state_ = PivotState::MOVING;
     }
-    else if(nav_state_ == NavState::MOVING){
+    else if(state_ == PivotState::MOVING){
       ROS_INFO_THROTTLE(2.0, "MOVING");
       geometry_msgs::Twist cmd_vel;
       if (local_planner_.isGoalReached()){
         ROS_INFO("reach");
-        twist_pub_.publish(cmd_vel);
-        nav_state_ = NavState::STANDBY;
+        vel_pub_.publish(cmd_vel);
+        state_ = PivotState::STANDBY;
         return;
       }
       local_planner_.computeVelocityCommands(cmd_vel);
-      twist_pub_.publish(cmd_vel);
+      vel_pub_.publish(cmd_vel);
     }
   }
-
+  
   ros::NodeHandle nh_;
-//#if ROS_VERSION_MINIMUM(1,14,0)
+  tf::TransformListener tf_;
   tf2_ros::Buffer tfBuffer;
   tf2_ros::TransformListener tfListener;
-//#endif
-  tf::TransformListener tf_; // obsolute
-  ros::Publisher twist_pub_;
+  ros::Publisher vel_pub_; //twist_pub_
   ros::Subscriber goal_sub_;
   ros::Timer timer_;
-
-  NavState nav_state_;
+  
+  PivotState state_; //nav_state_
   geometry_msgs::PoseStamped last_pose_;
   std::vector<geometry_msgs::PoseStamped> last_global_plan_;
-
+  
   costmap_2d::Costmap2DROS global_costmap_;
   costmap_2d::Costmap2DROS local_costmap_;
   navfn::NavfnROS global_planner_;
-  base_local_planner::TrajectoryPlannerROS local_planner_;
+  base_local_planner:: TrajectoryPlannerROS local_planner_;
 };
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "pivot_base");
-  PivotBase pivot_base; // class_name node_name create instance?
+  PivotBase pivot_base;
   ros::spin();
   return 0;
 }
